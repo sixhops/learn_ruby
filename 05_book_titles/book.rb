66481cdce1a423e74e1d54ba6845aed4df4343ef
Steve Peters <sixhops@gmail.com>
2016-08-04 12:26:11 -0700

    finished all tests

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,8 +1,31 @@
 class Book
 # write your code here
-	attr_accessor :title
+	def title=(t)
+		@title = titleize(t)
+	end
+	def title
+		@title
+	end
 
-	def initializer(t)
-		@title = t.capitalize!
+	def initializer(title)
+		@title = titleize(title)
+	end
+
+	private
+
+	def titleize(phrase)
+		words_to_not_capitalize = ["the", "and", "or", "in", "of", "a", "an"]
+		index = 0
+		past_first_word = false
+		words = phrase.split(" ")
+		words.each do |word|
+			word.capitalize!
+			words_to_not_capitalize.each do |w|
+				if (word.downcase == w) && (index > 0) then word.downcase! end
+			end
+			index += 1
+		end
+		phrase = words.join(" ")
+		return phrase
 	end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
   #write your code here
+	attr_accessor :seconds
+
+	def initialize
+  		@seconds = 0
+	end
+
+	def time_string
+		minutes = @seconds / 60
+		adjusted_seconds = @seconds - (minutes * 60)
+		hours = (minutes / 60)
+		adjusted_minutes = minutes - (hours * 60)
+		seconds_string = format("%02d", (@seconds - (hours * 60 * 60) - (minutes * 60)))
+		return format("%02d", hours) + ":" + format("%02d", adjusted_minutes) + ":" + format("%02d", adjusted_seconds)
+	end
 end
+
+t = Timer.new
+puts t.seconds
+puts t.time_string
+
+t.seconds = 12
+puts t.seconds
+puts t.time_string
+
+
